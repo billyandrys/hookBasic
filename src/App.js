bd0ea81af433048acd9f9f3d5757f57db77ce433
billyandryus <billyandrys@gmail.com>
2022-04-04 22:56:52 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { useReducer } from 'react' 
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const reducer = (state, action) =>{
-  switch(action.type){
-    case 'INCREMENT':
-      return {
-          count : state.count + 1,  showText : state.showText
-      }
-    case 'toggleShowText' :
-      return { 
-        count : state.count , showText: !state.showText
-      }
-    default :
-      return state
-
-  }
-}
 
 function App() {
-    const [state, dispatch ] = useReducer(reducer, { count : 0, showText : true })
-  return (
-    <div>
-       <h1>{ state.count }</h1>
-       <button onClick={()=>{
-         dispatch({type:'INCREMENT'})
-         dispatch({type:'toggleShowText'})
-       }}> Clic here </button>
-        {state.showText && <p> This is text </p>}
-    </div> 
-   
+
+  const [data, setData ] = useState('')
+  const [ count, setCount ] = useState(0)
+  useEffect(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/comments')
+        .then(response=>{
+          setData(response.data[count].email)
+          console.log("API WAS CALLED")
+        }, [count])}
   )  
+   
+    return (
+      <>
+      <h1>{ data }</h1>
+      <button onClick={()=>setCount(count + 1 )}>{count}</button>
+      </>
+
+    )
 }
 
 
