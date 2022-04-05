9cb08043e98f5ba5d00b5eaf538c037d403c4374
billyandryus <billyandrys@gmail.com>
2022-04-05 10:37:28 -0500

    useLayoutEffect example two

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 
-import { useLayoutEffect, useEffect } from 'react'
+import { useLayoutEffect, useEffect , useRef} from 'react'
+
 
 
 function App() {
+  const inputRef = useRef(null)
   useEffect(()=>{
-    console.log('useEffect')
+    inputRef.current.value = 'Hello'
   },[])
    
 
   useLayoutEffect(()=>{
-      console.log('UseLayoutEffect')
+     console.log(inputRef.current.value)
   }, []) 
   
   
     return (
       <>
-      
+      <input ref={inputRef} value='Pedro' style={{width:400, height:200}} />
       </>
 
     )
