9b714060071f8326b8db0eb5ee72f7cb31b97820
billyandryus <billyandrys@gmail.com>
2022-04-06 00:48:28 -0500

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 
 export default function App() {
-const [data, setData ] = useState(null)
-
+const [ data, setData ] = useState(null)
+const [ toggle, setToggle ]  = useState(false)
 useEffect(()=>{
       axios.get('https://jsonplaceholder.typicode.com/comments')
         .then((response)=>{
@@ -19,18 +19,26 @@ const findLongestName = ( comments )=>{
     let currentName = comments[i].name
     if(currentName.length > longestName.length){
       longestName = currentName
-      console.log(longestName)
+      
     }
   }
 
   console.log('this was computed')
   return longestName 
 }
+const getLongesName = useMemo(()=>findLongestName(data), [data])
 
   return (
     <>
-      <div>{findLongestName(data)}</div>
-      
+      <div>{getLongesName}</div>
+      <button onClick={(e)=>{
+        
+        setToggle(!toggle)
+      }}>
+        {''}
+        Toogle
+      </button>
+      {toggle && <h1>Toogle</h1>}
     </>
   )
     
